945fd0c3a451961dfc0ad56df6d3ffdc7345db41
Christian Binzer <cbinzer@users.noreply.github.com>
2023-06-19 21:39:37 +0200

    refactor(entry): Use common modal in deleteEntryModal

--- islands/deleteEntryModal.tsx
+++ islands/deleteEntryModal.tsx
@@ -1,55 +1,34 @@
 import { FunctionComponent } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
 import { deleteEntryByCursor } from '../lib/entry/entryClientService.ts';
 import { Entry } from '../lib/entry/models.ts';
+import Modal from './modal.tsx';
+import { convertKeyToString } from '../lib/entry/utils.ts';
 
 const DeleteEntryModal: FunctionComponent<DeleteEntryModalProps> = (
   { open = false, entry, onClose = () => {}, onDelete = () => {} },
 ) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
-
   const deleteEntry = async () => {
     await deleteEntryByCursor(entry?.cursor as string);
-    onDelete();
-    closeModal();
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
     onClose();
+    setTimeout(() => onDelete(), 150);
   };
-
-  useEffect(() => {
-    setIsOpen(open);
-  }, [open]);
-
-  useEffect(() => {
-    setIsVisible(open);
-  }, [isOpen]);
 
   return (
-    <div
-      class={`delete-entry-modal modal fade ${isVisible ? 'show' : ''}`}
-      tabIndex={-1}
-      style={{ display: isOpen ? 'block' : undefined }}
-    >
-      <div class='modal-dialog'>
-        <div class='modal-content'>
-          <div class='modal-header'>
-            <h1 class='modal-title fs-5'>Delete entry</h1>
-            <button class='btn-close' onClick={closeModal}></button>
-          </div>
-          <div class='modal-body'>
-            <p>Do you wanna really delete the entry with key [{entry?.key.join(', ')}]?</p>
-          </div>
-          <div class='modal-footer'>
-            <button class='btn btn-secondary' onClick={closeModal}>Cancel</button>
-            <button class='btn btn-danger' onClick={deleteEntry}>Delete</button>
-          </div>
-        </div>
+    <Modal open={open} onClose={onClose}>
+      <div class='modal-header'>
+        <h1 class='modal-title fs-5'>Delete entry</h1>
+        <button class='btn-close' onClick={onClose}></button>
       </div>
-    </div>
+
+      <div class='modal-body'>
+        <p>Do you wanna really delete the entry with key [{convertKeyToString(entry.key)}]?</p>
+      </div>
+
+      <div class='modal-footer'>
+        <button class='btn btn-secondary' onClick={onClose}>Cancel</button>
+        <button class='btn btn-danger' onClick={deleteEntry}>Delete</button>
+      </div>
+    </Modal>
   );
 };
 
