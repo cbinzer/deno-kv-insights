1d89cd6c0aa6d22c7c58c52bf9363cd5bb1fb4f9
Christian Binzer <cbinzer@users.noreply.github.com>
2023-06-27 23:34:20 +0200

    feat(entry): Add encode cursor for deno deploy

--- lib/entry/entryRepository.ts
+++ lib/entry/entryRepository.ts
@@ -4,7 +4,7 @@ import { VersionConflictError } from '../common/errors.ts';
 import { Pagination } from '../common/models.ts';
 
 // @ts-ignore
-console.log(Deno[Deno.internal])
+console.log(Deno[Deno.internal].core)
 
 export async function findAllEntries(filter?: EntryFilter, pagination?: Pagination): Promise<CursorBasedDBEntry[]> {
   const entries: CursorBasedDBEntry[] = [];
