1ff9042cfbeebf5041330554065156fbdebb4244
Christian Binzer <cbinzer@users.noreply.github.com>
2023-06-28 09:57:10 +0200

    chore: Add logs

--- lib/common/db.ts
+++ lib/common/db.ts
@@ -1,3 +1,8 @@
 export const db = await Deno.openKv();
 
-console.log(Object.keys(Deno).forEach(key => console.log(key)));
+console.log('db keys:')
+console.log(Object.keys(db).forEach(key => console.log(key)));
+
+console.log('Kv keys:')
+//@ts-ignore
+console.log(Object.keys(Deno.Kv).forEach(key => console.log(key)));
