4ed9e7fe84955a13bf4d99f8034ba963cc1fffd4
Christian Binzer <cbinzer@users.noreply.github.com>
2023-07-08 14:31:41 +0200

    chore: Add initial mod.ts

---
+++ mod.ts
@@ -0,0 +1,4 @@
+import EntriesManagement from './lib/entry/islands/entriesManagement.tsx';
+
+export { config, default, handler } from './lib/entry/routes/kvInsightsRoute.tsx';
+export { EntriesManagement };
