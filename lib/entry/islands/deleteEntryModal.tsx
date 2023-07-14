79ca6672d9f13d07335624afaab0e48983c81f19
Christian Binzer <cbinzer@users.noreply.github.com>
2023-07-14 23:54:18 +0200

    feat(entry): Add loading spinner to delete entry modal

--- lib/entry/islands/deleteEntryModal.tsx
+++ lib/entry/islands/deleteEntryModal.tsx
@@ -3,12 +3,17 @@ import { deleteEntryByCursor } from '../entryClientService.ts';
 import { Entry } from '../models.ts';
 import Modal from '../../common/islands/modal.tsx';
 import { convertKeyToString } from '../utils.ts';
+import { useState } from 'https://esm.sh/stable/preact@10.15.1/denonext/hooks.js';
 
 const DeleteEntryModal: FunctionComponent<DeleteEntryModalProps> = (
   { open = false, entry, onClose = () => {}, onDelete = () => {} },
 ) => {
+  const [isDeleting, setIsDeleting] = useState(false);
   const deleteEntry = async () => {
+    setIsDeleting(true);
     await deleteEntryByCursor(entry?.cursor as string);
+    setIsDeleting(false);
+
     onClose();
     setTimeout(() => onDelete(), 150);
   };
@@ -25,8 +30,10 @@ const DeleteEntryModal: FunctionComponent<DeleteEntryModalProps> = (
       </div>
 
       <div class='modal-footer'>
-        <button class='btn btn-secondary' onClick={onClose}>Cancel</button>
-        <button class='btn btn-danger' onClick={deleteEntry}>Delete</button>
+        <button class='btn btn-secondary' onClick={onClose} disabled={isDeleting}>Cancel</button>
+        <button class='btn btn-danger' onClick={deleteEntry} disabled={isDeleting}>
+          {isDeleting ? <span class='spinner-border spinner-border-sm' /> : 'Delete'}
+        </button>
       </div>
     </Modal>
   );
