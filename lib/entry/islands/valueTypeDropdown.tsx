bb7b8d51459cf8b3f083d6ae125a591306157302
Christian Binzer <cbinzer@users.noreply.github.com>
2023-07-15 00:40:20 +0200

    feat(entry): Close value type dropdown menu on outside click

--- lib/entry/islands/valueTypeDropdown.tsx
+++ lib/entry/islands/valueTypeDropdown.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from 'preact';
-import { useState } from 'preact/hooks';
+import { useEffect, useMemo, useRef, useState } from 'preact/hooks';
 import { ValueType } from '../models.ts';
 import { getValueTypeColorClass } from '../utils.ts';
 
@@ -7,6 +7,21 @@ const ValueTypeDropdown: FunctionComponent<ValueTypeDropdownProps> = (
   { valueType = ValueType.STRING, onSelect = () => {} },
 ) => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    if (menuVisible) {
+      document.addEventListener('mousedown', closeMenuOnOutsideClick);
+    } else {
+      document.removeEventListener('mousedown', closeMenuOnOutsideClick);
+    }
+  }, [menuVisible]);
+
+  const closeMenuOnOutsideClick = useMemo(() => (event: Event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuVisible(false);
+    }
+  });
 
   const changeSelectedValueType = (event: Event, valueType: ValueType) => {
     event.preventDefault();
@@ -34,7 +49,7 @@ const ValueTypeDropdown: FunctionComponent<ValueTypeDropdownProps> = (
       >
         {valueType}
       </button>
-      <ul class={`dropdown-menu ${menuVisible ? 'show' : ''}`} style={{ top: '35px' }}>
+      <ul class={`dropdown-menu ${menuVisible ? 'show' : ''}`} style={{ top: '35px' }} ref={menuRef}>
         {getAllValueTypes().map((valueType) => (
           <li>
             <a class='dropdown-item' href='#' onClick={(event) => changeSelectedValueType(event, valueType)}>
