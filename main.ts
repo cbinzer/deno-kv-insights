6dea416a8cd0c9cffabcc22cfd135b552db64c9c
Christian Binzer <cbinzer@users.noreply.github.com>
2023-07-16 20:53:25 +0200

    chore: Add more data to deploy

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import { start } from '$fresh/server.ts';
 import manifest from './fresh.gen.ts';
 import { db } from './lib/common/db.ts';
 
-for (let i = 700; i < 1100; i++) {
+for (let i = 1100; i < 1600; i++) {
   await db.set(['test', i], {});
 }
 
