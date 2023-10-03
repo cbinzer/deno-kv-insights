6bdd51e6f220423b1785c0c0011a173f0fecbad8
Christian Binzer <cbinzer@users.noreply.github.com>
2023-10-03 14:05:59 +0200

    feat(queue): Add subscribe button

--- lib/queue/islands/queueManagement.tsx
+++ lib/queue/islands/queueManagement.tsx
@@ -1,10 +1,20 @@
 import { FunctionComponent } from 'preact';
 
 const QueueManagement: FunctionComponent = () => {
-  return <div class="queue-management">
-    <div class="panel">Panel 1</div>
-    <div class="panel">Panel 2</div>
-  </div>;
+  return (
+    <div class='queue-management'>
+      <div class='panel'>
+        <div class='top-panel-container'>
+          <div class='action-container'>
+            <button class='btn btn-primary float-end'>Subscribe</button>
+          </div>
+          <div class='messages-container'></div>
+        </div>
+      </div>
+
+      <div class='panel'>Panel 2</div>
+    </div>
+  );
 };
 
 export default QueueManagement;
