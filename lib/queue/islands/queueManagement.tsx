5ab715aa563938149cb792e0f84d93aaec24f610
Christian Binzer <cbinzer@users.noreply.github.com>
2023-10-03 13:01:41 +0200

    feat(queue): Add initial queue management component

---
+++ lib/queue/islands/queueManagement.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'preact';
+
+const QueueManagement: FunctionComponent = () => {
+  return <div class="queue-management">
+    <div class="panel">Panel 1</div>
+    <div class="panel">Panel 2</div>
+  </div>;
+};
+
+export default QueueManagement;
--- lib/queue/routes/queueRoute.tsx
+++ lib/queue/routes/queueRoute.tsx
@@ -1,9 +1,10 @@
 import Page from '../../common/components/page.tsx';
+import QueueManagement from '../islands/queueManagement.tsx';
 
 export function QueuePageRoute() {
   return (
     <Page currentRoute={'/kv-insights/queue'}>
-      <div>Hallo Welt!</div>
+      <QueueManagement />
     </Page>
   );
 }
